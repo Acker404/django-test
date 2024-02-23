b900614b5737c118ac0a87e6203c4c10839d1ef1
Acker404 <mark921202@gmail.com>
2024-02-23 16:36:46 +0800

    1.0

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from mysite.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_data')
+admin.site.register(Post,PostAdmin)
